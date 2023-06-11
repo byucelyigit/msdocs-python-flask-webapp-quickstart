17133d2497a058f083d5e7771fced58872ac9bfa
Burak Yücelyiğit <burak.yucelyigit@enocta.com>
2023-06-11 13:53:50 +0300

    standart web sayfası çalışma denemesi 1

--- app.py
+++ app.py
@@ -1,73 +1,42 @@
-import azure.cosmos.documents as documents
-import azure.cosmos.cosmos_client as cosmos_client
-import azure.cosmos.exceptions as exceptions
-from azure.cosmos.partition_key import PartitionKey
-import datetime
-
+import os
 import config
 
-# ----------------------------------------------------------------------------------------------------------
-# Prerequistes -
-#
-# 1. An Azure Cosmos account -
-#    https://docs.microsoft.com/azure/cosmos-db/create-cosmosdb-resources-portal#create-an-azure-cosmos-db-account
-#
-# 2. Microsoft Azure Cosmos PyPi package -
-#    https://pypi.python.org/pypi/azure-cosmos/
-# ----------------------------------------------------------------------------------------------------------
-# Sample - demonstrates the basic CRUD operations on a Item resource for Azure Cosmos
-# ----------------------------------------------------------------------------------------------------------
 
+from flask import (Flask, redirect, render_template, request,
+                   send_from_directory, url_for)
+
+app = Flask(__name__)
+
+root_dir = "c:\\Users\\burak.yucelyigit\\OneDrive\\kisisel\Yazılım\\openai\\crawler\\SemanticCatalogSearch\\"
 HOST = config.settings['host']
 MASTER_KEY = config.settings['master_key']
 DATABASE_ID = config.settings['database_id']
 CONTAINER_ID = config.settings['container_id']
 
 
+@app.route('/')
+def index():
+   print('Request for index page received')
+   return render_template('index.html')
 
-def read_items(container):
-    print('\nReading all items in a container\n')
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-    # NOTE: Use MaxItemCount on Options to control how many items come back per trip to the server
-    #       Important to handle throttles whenever you are doing operations such as this that might
-    #       result in a 429 (throttled request)
-    item_list = list(container.read_all_items(max_item_count=10))
-
-    print('Found {0} items'.format(item_list.__len__()))
-
-    for doc in item_list:
-        print('Item Id: {0}'.format(doc.get('id')))
-    return
-
-
-def query_items(container, account_number):
-    print('\nQuerying for an  Item by Partition Key\n')
-
-    # Including the partition key value of account_number in the WHERE filter results in a more efficient query
-    items = list(container.query_items(
-        query="SELECT * FROM r WHERE r.partitionKey=@account_number",
-        parameters=[
-            { "name":"@account_number", "value": account_number }
-        ]
-    ))
-    print('Item queried by Partition Key {0}'.format(items[0].get("id")))
-    return
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
+   if name:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('hello.html', name = ReturnSomething())
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
 
 
-
-
-def run_sample():
-    client = cosmos_client.CosmosClient(HOST, {'masterKey': MASTER_KEY}, user_agent="CosmosDBPythonQuickstart", user_agent_overwrite=True)
-    try:
-        db = client.get_database_client(DATABASE_ID)
-        container = db.get_container_client(CONTAINER_ID)
-        read_items(container)
-
-
-    except exceptions.CosmosHttpResponseError as e:
-        print('\nrun_sample has caught an error. {0}'.format(e.message))
-    finally:
-            print("\nrun_sample done")
+def ReturnSomething():
+    return "result"
 
 if __name__ == '__main__':
-    run_sample()
+   app.run()
---
+++ app_v0.py
@@ -0,0 +1,73 @@
+import azure.cosmos.documents as documents
+import azure.cosmos.cosmos_client as cosmos_client
+import azure.cosmos.exceptions as exceptions
+from azure.cosmos.partition_key import PartitionKey
+import datetime
+
+import config
+
+# ----------------------------------------------------------------------------------------------------------
+# Prerequistes -
+#
+# 1. An Azure Cosmos account -
+#    https://docs.microsoft.com/azure/cosmos-db/create-cosmosdb-resources-portal#create-an-azure-cosmos-db-account
+#
+# 2. Microsoft Azure Cosmos PyPi package -
+#    https://pypi.python.org/pypi/azure-cosmos/
+# ----------------------------------------------------------------------------------------------------------
+# Sample - demonstrates the basic CRUD operations on a Item resource for Azure Cosmos
+# ----------------------------------------------------------------------------------------------------------
+
+HOST = config.settings['host']
+MASTER_KEY = config.settings['master_key']
+DATABASE_ID = config.settings['database_id']
+CONTAINER_ID = config.settings['container_id']
+
+
+
+def read_items(container):
+    print('\nReading all items in a container\n')
+
+    # NOTE: Use MaxItemCount on Options to control how many items come back per trip to the server
+    #       Important to handle throttles whenever you are doing operations such as this that might
+    #       result in a 429 (throttled request)
+    item_list = list(container.read_all_items(max_item_count=10))
+
+    print('Found {0} items'.format(item_list.__len__()))
+
+    for doc in item_list:
+        print('Item Id: {0}'.format(doc.get('id')))
+    return
+
+
+def query_items(container, account_number):
+    print('\nQuerying for an  Item by Partition Key\n')
+
+    # Including the partition key value of account_number in the WHERE filter results in a more efficient query
+    items = list(container.query_items(
+        query="SELECT * FROM r WHERE r.partitionKey=@account_number",
+        parameters=[
+            { "name":"@account_number", "value": account_number }
+        ]
+    ))
+    print('Item queried by Partition Key {0}'.format(items[0].get("id")))
+    return
+
+
+
+
+def run_sample():
+    client = cosmos_client.CosmosClient(HOST, {'masterKey': MASTER_KEY}, user_agent="CosmosDBPythonQuickstart", user_agent_overwrite=True)
+    try:
+        db = client.get_database_client(DATABASE_ID)
+        container = db.get_container_client(CONTAINER_ID)
+        read_items(container)
+
+
+    except exceptions.CosmosHttpResponseError as e:
+        print('\nrun_sample has caught an error. {0}'.format(e.message))
+    finally:
+            print("\nrun_sample done")
+
+if __name__ == '__main__':
+    run_sample()
