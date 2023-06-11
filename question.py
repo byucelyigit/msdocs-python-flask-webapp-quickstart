c60ec8336bb144dc8fbcbbf4871c45dfa9e4bd0f
Burak Yücelyiğit <burak.yucelyigit@enocta.com>
2023-06-11 20:49:11 +0300

    question işlemi farklı değerler buluyor

--- question.py
+++ question.py
@@ -19,6 +19,7 @@ from dotenv import load_dotenv
 # ilk olarak azure_cli dizinindeki oluşturma cli komutları çalıştırılır. 
 # bu komutlar Azure tarafındaki ortamı hazırlar.
 # iş bitince yine aynı dizindeki resourceları kaldıran komut çalıştırılır.
+# azure kaynakları hazır olunca init.py ile DB oluşturma ve embeddings export işlemleri yapılır.
 
 
 def query_items(id):
@@ -38,23 +39,7 @@ def query_items(id):
 
     print('Item queried by id  {0}'.format(items[0].get("id")))
 
-def CreateEmbeddingsVectorIndex():
-    # Connect to the Cosmos DB using the Azure Cosmos Client
-    client = pymongo.MongoClient(MONGO_CONNECTION_STRING )
-    db = client.get_database(DATABASE_ID)
 
-    index_spec = [
-    ('vectorContent', 'text')  # Specify the field and its type in a tuple
-    ]
-
-    # Create the index
-    db[COLLECTION_ID_EMBEDDINGS].create_index(index_spec, name='vectorSearchIndex', cosmosSearchOptions={
-        'kind': 'vector-ivf',
-        'numLists': 100,
-        'similarity': 'COS',
-        'dimensions': 1536
-    })
-    print("Index created successfully")
 
 def deletemongodbrecords():
     # Connect to the Cosmos DB using the Azure Cosmos Client
@@ -74,50 +59,6 @@ def deletemongodbrecords():
 
 #-------------------------------------------------------------------------
 #yukarısı deneysel kodlar
-
-def CreateEmbeddingsDatabase():
-    client = pymongo.MongoClient(MONGO_CONNECTION_STRING )
-    db=client[DATABASE_ID]
-    db.create_collection(COLLECTION_ID_EMBEDDINGS)
-
-def ReadEmbeddings():
-    df_embeddings = pd.read_csv('embeddings_eng.csv')    
-    # write a code that show current directory
-    print(os.getcwd())
-    # write a code that list first four items of df_embeddings
-    print(df_embeddings.head())
-    # write a code that show the number of rows and columns of df_embeddings
-    print(df_embeddings.shape)
-    return df_embeddings
-
-# aşağıdaki fonksiyonu  bir kez çalıştırmak lazım. 
-# embeddings csv dosyasındaki embeddings verilerini cosmodb'ye yazar.
-def ExportEmbeddings():
-    print("ExportEmbeddings")
-    embeddingList = ReadEmbeddings()
-    CreateEmbedingItems(embeddingList) 
-
-#
-def CreateEmbedingItems(embeddingList):
-    print("CreateEmbedingItems")
-    client = pymongo.MongoClient(MONGO_CONNECTION_STRING)
-    db = client.get_database(DATABASE_ID)
-    # collection = db['Catalog']
-    # limited_df = embeddingList.head(2)
-    for index, row in embeddingList.iterrows():
-        # convert index into a string
-        i = str(index+1)
-        print("index:") 
-        print(i)
-        embeding = EmbeddingItem(row['index'], row['embedding'])
-        db[COLLECTION_ID_EMBEDDINGS].insert_one(embeding)
-        print("item created")
-
-def EmbeddingItem(index, embedding):
-    embedding_array = np.array(eval(embedding))
-    item = {'id' : index,
-            'vectorContent' : embedding_array.tolist()}
-    return item
 
 def GenerateQuestionEmbeddings():
     # aşağıdaki ap_key bilgisinin gihub'a gönderilmiyor olması lazım. 
@@ -158,8 +99,8 @@ def GenerateQuestionEmbeddings():
     print("Search resut:")
     for document in result:
         print(document['id'])
-        r = document['id']
-    return r
+        # r = document['id']
+    return 
 
 def deleteEmbeddings():
     print("Connect to mongodb")
@@ -168,30 +109,6 @@ def deleteEmbeddings():
     client.drop_database(DATABASE_ID)
     print("Database dropped")
 
-def CreateCourse():
-    client = pymongo.MongoClient(MONGO_CONNECTION_STRING )
-    db=client[DATABASE_ID]
-    db.create_collection(COLLECTION_ID_CATALOG_DESC)
-    df_coursedata = pd.read_csv('katalog.csv', sep=';')    
-    # write a code that show current directory
-    print(os.getcwd())
-    # write a code that list first four items of df_embeddings
-    print(df_coursedata.head())
-    # write a code that show the number of rows and columns of df_embeddings
-    print(df_coursedata.shape)
-
-    for index, row in df_coursedata.iterrows():
-        # convert index into a string
-        i = str(index+1)
-        print("index:") 
-        print(i)
-        item = {'id'     : row['index'],
-                'header' : row['header'],
-                'desc'   :row['desc']
-            }
-        db[COLLECTION_ID_CATALOG_DESC].insert_one(item)
-        print("item created")
-    return
 
 # index listesi verilen kursların bilgilerini mongodb'den bulup df olarak döner.
 def Context(df_indexes):
@@ -205,23 +122,6 @@ def Context(df_indexes):
     ))
 
     print('Item queried by id  {0}'.format(items[0].get("id"))) 
-
-def Init():
-    # --------------------------------------------
-    # aşağıdaki işlemlerden önce ilgili azure resource tanımlarının yapılmış olması gerekir. Bunun için azurecli dizinindeki dosya kullanılır.
-    # Aşağıdaki kısım bir kez çalıştırılır.
-    # 1.
-    # azure cli komutları ile resource oluşturulur.
-    # create database and collection
-    # 2.
-    CreateEmbeddingsDatabase()
-    # 3.
-    ExportEmbeddings()  
-    # 4. 
-    CreateEmbeddingsVectorIndex()
-    # --------------------------------------------
-    CreateCourse()
-
 
 MONGO_CONNECTION_STRING = ""
 DATABASE_ID = "EnoctaSemanticSearch"
@@ -246,14 +146,13 @@ def LoadEnvVariables():
     # print(OPENAI_ORG_ID)
     # print(OPENAI_APIKEY)
 
-#LoadEnvVariables()
+LoadEnvVariables()
 #result = Context([710, 68])
 #print(result)
 #deleteEmbeddings()
-Init()
 # ReadEmbeddingsFromCosmoDB()  # nosql için  >700 maddeyi çekmesi epey vakit alıyor. mongodb içn biraz daha hızlı 
 # searchmongodb()
-# GenerateQuestionEmbeddings()
+GenerateQuestionEmbeddings()
 # createmongodbvectorindex()
 # deletemongodbrecords()
 # query_items("1")
